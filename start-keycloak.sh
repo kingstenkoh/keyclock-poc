e821897529d9b1f7cc893df3cd4e4560ee7e7e9e
Kingsten <kingstenkoh@gmail.com>
2020-04-03 16:37:35 +0800

    add docker build and docke run

--- start-keycloak.sh
+++ start-keycloak.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 8180:8180 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e JAVA_OPTS=-Djboss.socket.binding.port-offset=100 quay.io/keycloak/keycloak:9.0.2
+sudo docker run -p 8180:8180 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e "JAVA_OPTS=-Djboss.socket.binding.port-offset=100 -Dkeycloak.profile.feature.upload_scripts=enabled" quay.io/keycloak/keycloak:9.0.2
